4e8ee1f0b4b8148c8321393e6d863adcc29ec2fd
Alessandro De Maria <a.demaria@synthace.com>
2020-10-28 11:11:47 +0000

    fix build;

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,9 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
+go build -tags "nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+
+# compile the runners with gcc disabled
+export CGO_ENABLED=0
+go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
+go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
